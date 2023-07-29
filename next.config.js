716633f1e4c16173303959a283da617fc0b9e9c9
Alaa Barka <tmakaveli643@gmail.com>
2023-07-29 12:52:08 +0100

    more datas

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        domains:['stablo-template.vercel.app', 'cdn.sanity.io','www.w3schools.com', 'plus.unsplash.com']
+        domains:['stablo-template.vercel.app', 'cdn.sanity.io','www.w3schools.com', 'plus.unsplash.com','cdn.pixabay.com']
     }
 }
 
