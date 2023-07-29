c51765e9c3a2ca79609f7b84df05c6b19883f6da
Alaa Barka <tmakaveli643@gmail.com>
2023-07-29 17:07:46 +0100

    cleaning code from comments and semicols

--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,47 @@
 "use client"
 
-import React, { useState, useRef, useLayoutEffect } from 'react';
+import React, { useState, useRef, useLayoutEffect } from 'react'
 import Navbar from "./components/navbar";
 import Head from "next/head";
 import Blog from "./components/Blog";
 import axios from 'axios';
-import InfiniteScroll from 'react-infinite-scroll-component';
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 const getBlogs = async (pg:any) => {
   try {
-    const res = await axios.get(`https://blogtst.vercel.app/api/all?pg=${pg}`);
-    return res.data;
+    const res = await axios.get(`https://blogtst.vercel.app/api/all?pg=${pg}`)
+    return res.data
   } catch (error) {
-    console.error("Error fetching data:", error);
-    return [];
+    console.error("Error fetching data:", error)
+    return []
   }
 };
 
 const Home = () => {
-  const [pg, setPg] = useState(0);
-  const [blogs, setBlogs]:any = useState([]);
-  const [blockDimensions, setBlockDimensions]:any = useState({}); // Store width and height of each block
+  const [pg, setPg] = useState(0)
+  const [blogs, setBlogs]:any = useState([])
+  const [blockDimensions, setBlockDimensions]:any = useState({})
 
   const fetchMoreBlogs = async () => {
-    const nextPage = pg + 1;
-    const newBlogs = await getBlogs(nextPage);
+    const nextPage = pg + 1
+    const newBlogs = await getBlogs(nextPage)
 
-    // Check if the API response is empty, and if it is, set hasMore to false
     if (newBlogs.length === 0) {
-      setPg(-1); // Set pg to a value that indicates there are no more items to fetch
+      setPg(-1)
     } else {
-      setBlogs((prevBlogs:any) => [...prevBlogs, ...newBlogs]);
+      setBlogs((prevBlogs:any) => [...prevBlogs, ...newBlogs])
       setPg(nextPage);
     }
   };
 
-  // Create a ref to track the InfiniteScroll container
-  const infiniteScrollRef = useRef(null);
+  const infiniteScrollRef = useRef(null)
   React.useEffect(() => {
-    // Fetch first set of elements
-    fetchMoreBlogs();
-  }, []);
+    fetchMoreBlogs()
+  }, [])
 
-  // Use useLayoutEffect to check the dimensions of each block after rendering
   useLayoutEffect(() => {
     const updateBlockDimensions = () => {
-      const blocks:any = document.getElementsByClassName('blog-block');
+      const blocks:any = document.getElementsByClassName('blog-block')
       const dimensions:any = {};
       for (let i = 0; i < blocks.length; i++) {
         const block:any = blocks[i];
@@ -55,22 +51,19 @@ const Home = () => {
           height: rect.height,
         };
       }
-      setBlockDimensions(dimensions);
+      setBlockDimensions(dimensions)
     };
 
-    // Call the function after the render is committed to the screen
-    updateBlockDimensions();
+    updateBlockDimensions()
 
-    // Listen for window resize events to update block dimensions if needed
-    window.addEventListener('resize', updateBlockDimensions);
+    window.addEventListener('resize', updateBlockDimensions)
 
-    // Clean up event listener on component unmount
     return () => {
-      window.removeEventListener('resize', updateBlockDimensions);
-    };
-  }, [blogs]); // Run this effect whenever the blogs state changes
+      window.removeEventListener('resize', updateBlockDimensions)
+    }
+  }, [blogs])
 
-  console.log("test", blogs);
+  console.log("test", blogs)
 
   return !blogs ? (
     <>Loading</>
@@ -80,14 +73,14 @@ const Home = () => {
         <InfiniteScroll
           dataLength={blogs.length}
           next={fetchMoreBlogs}
-          hasMore={pg !== -1} // Set to false when there are no more items to fetch
-          loader={<h4 className='text-center'>Loading...</h4>} // Loading indicator shown while new data is being fetched
-          endMessage={<h4>No more items to load.</h4>} // Message shown when all items have been loaded
-          scrollThreshold={0.9} // Percentage of the scrollable area that triggers the next fetch
+          hasMore={pg !== -1} 
+          loader={<h4 className='text-center'>Loading...</h4>} 
+          endMessage={<h4>No more items to load.</h4>} 
+          scrollThreshold={0.9} 
           style={{
             width: "100%",
           }}
-          ref={infiniteScrollRef} // Assign the ref to the InfiniteScroll container
+          ref={infiniteScrollRef}
         >
           <div className="w-full  m-auto flex flex-wrap gap-y-10">
             {blogs.map((blog:any, index:any) => (
@@ -103,7 +96,7 @@ const Home = () => {
           </div>
         </InfiniteScroll>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
